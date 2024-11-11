c4574ba7be3f086c1a4eee9a8ccc699092fa0232
Takahashi Shota <shohh6119@gmial.com>
2024-11-11 16:37:02 +0900

    Impl GeminiRequester

--- app/request_gemini.py
+++ app/request_gemini.py
@@ -1,9 +1,23 @@
-import sys
-
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()  # Load environment variables from .env file
+
+import google.generativeai as genai
 import os
 
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-sys.stdout.write(GEMINI_API_KEY)
+
+class GeminiRequester:
+    def __init__(self):
+        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+    def request(self, text: str) -> str:
+        response = self.model.generate_content(text)
+        return response.text
+
+
+# Usage example
+if __name__ == "__main__":
+    requester = GeminiRequester()
+    response_text = requester.request("こんにちは。")
+    print(response_text)
